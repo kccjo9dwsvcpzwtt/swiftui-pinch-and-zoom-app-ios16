09c5ea6577dd82db5e7ede61b4aece10919fafc2
kccjo9dwsvcpzwtt <128245629+kccjo9dwsvcpzwtt@users.noreply.github.com>
2023-12-14 00:49:34 +0200

    Added Front-Image & Tap-Gesture

--- swiftui-pinch-and-zoom-app-ios16/Screens/ContentView.swift
+++ swiftui-pinch-and-zoom-app-ios16/Screens/ContentView.swift
@@ -11,23 +11,54 @@ struct ContentView: View {
   
   //MARK: - PROPERTY
   
+  @State private var isAnimating: Bool = false
+  @State private var imageScale: CGFloat = 1
+  
   //MARK: - FUNCTION
   
   //MARK: - CONTENT
   
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    NavigationStack {
+      ZStack {
+        
+        //MARK: - PAGE IMAGE
+        
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
+          .scaleEffect(imageScale)
+        //MARK: - 1. TAP GESTURE
+          .onTapGesture(count: 2, perform: {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5 // SCALE-UP
+              }
+            } else {
+              withAnimation(.spring()) {
+                imageScale = 1 // BACK TO DEFAULT
+              }
+            }
+          })
+        
+      } //: ZSTACK
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
+      .onAppear(perform: {
+        withAnimation(.linear(duration: 1)) {
+          isAnimating = true
         }
-        .padding()
-    }
+      })
+    } //: NAVIGATION STACK
+    .navigationViewStyle(.stack)
+  }
 }
 
 //MARK: - PREVIEW
 
 #Preview {
-    ContentView()
+  ContentView()
 }
--- swiftui-pinch-and-zoom-app-ios16/swiftui_pinch_and_zoom_app_ios16App.swift
+++ swiftui-pinch-and-zoom-app-ios16/swiftui_pinch_and_zoom_app_ios16App.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct swiftui_pinch_and_zoom_app_ios16App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
