da221c22a1d8e3d28acc264cd898507fcb0bb30c
kccjo9dwsvcpzwtt <128245629+kccjo9dwsvcpzwtt@users.noreply.github.com>
2023-12-15 17:35:11 +0200

    Add Drag Gesture

--- swiftui-pinch-and-zoom-app-ios16/Screens/ContentView.swift
+++ swiftui-pinch-and-zoom-app-ios16/Screens/ContentView.swift
@@ -13,8 +13,19 @@ struct ContentView: View {
   
   @State private var isAnimating: Bool = false
   @State private var imageScale: CGFloat = 1
+  /*
+   @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
+   */
+  @State private var imageOffset: CGSize = .zero // OPTIMIZATION OF PREVIOUS PROPERTY
   
   //MARK: - FUNCTION
+  
+  func resetImageState() {
+    return withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
   
   //MARK: - CONTENT
   
@@ -30,19 +41,36 @@ struct ContentView: View {
           .cornerRadius(10)
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
           .opacity(isAnimating ? 1 : 0)
+          .offset(x: imageOffset.width, y: imageOffset.height)
           .scaleEffect(imageScale)
+        
         //MARK: - 1. TAP GESTURE
+        
           .onTapGesture(count: 2, perform: {
             if imageScale == 1 {
               withAnimation(.spring()) {
                 imageScale = 5 // SCALE-UP
               }
             } else {
-              withAnimation(.spring()) {
-                imageScale = 1 // BACK TO DEFAULT
-              }
+              resetImageState()
             }
           })
+        
+        //MARK: - 2. DRAG GESTURE
+        
+          .gesture(
+            DragGesture()
+              .onChanged({ value in
+                withAnimation(.linear(duration: 1)) {
+                  imageOffset = value.translation
+                }
+              })
+              .onEnded({ _ in
+                if imageScale <= 1 {
+                  resetImageState()
+                }
+              })
+          )
         
       } //: ZSTACK
       .navigationTitle("Pinch & Zoom")
